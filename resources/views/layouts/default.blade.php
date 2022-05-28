ef2ee9cca294d55c69415affb442549d6938cf97
masaharu yoshikawa <progressmy@p2087051-ipngn200608osakachuo.osaka.ocn.ne.jp>
2022-05-28 21:29:06 +0900

    2nd_commit

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -6,6 +6,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>@yield('title')</title>
+  <link rel="stylesheet" href="{{ asset('css/style.css') }}">
   <style>
     body {
       font-size: 16px;
@@ -13,7 +14,8 @@
     }
 
     h1 {
-      font-size: 60px;
+      font-size: 30px;
+      text-align: center;
     }
 
 
